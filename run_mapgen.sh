dab876df0502068448e4f6da3254e7e8500a31fd
Israel Brewster <ijbrewster@alaska.edu>
2022-01-06 08:52:55 -0900

    - rename main.js to prevent cache issues - update run script for new deployment location - add original .svg files for tiltmeter and webcam

--- run_mapgen.sh
+++ run_mapgen.sh
@@ -2,6 +2,5 @@
 
 source ~/.bashrc
 conda activate mapgen
-cd /home/mapgen/MapGen
-/usr/local/miniconda3/envs/mapgen/bin/python -u /home/mapgen/MapGen/run_mapgen.py
-
+cd /data/MapGen
+/usr/local/miniconda3/envs/mapgen/bin/python -u /data/MapGen/run_mapgen.py
